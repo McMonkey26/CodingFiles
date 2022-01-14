c5871b58757de7b105e0d53506a005d8c9e71a15
McMonkey26 <julian@pollackfamily.net>
2022-01-13 23:27:05 -0500

    Created tool class

--- discordBots/minecord/textBasedDemo.py
+++ discordBots/minecord/textBasedDemo.py
@@ -53,17 +53,28 @@ class Item:
   def show(self):
     print(vars(self))
 
-class Player:
-  def __init__(self):
-    self.money = 0
 class Tool:
-  def __init__(self, tool, level, harvests):
-    self.type = tool
-    self.level = level
+  def __init__(self, name, harvests):
+    self.name = name
+    self.type = name.split()[1].lower()
+    self.level = name.split()[0].lower()
     self.harvests = harvests
   def use(self):
     for material in harvests:
       material.harvest()
+woodPickaxe = Tool('Wood Pickaxe', [stone])
+stonePickaxe = Tool('Stone Pickaxe', [stone])
+class Player:
+  def __init__(self):
+    self.money = 0
+    self.pick = woodPickaxe
+  def upgrade(self, type, tool, cost):
+    if cost > self.money:
+      return None
+    switch(type)
+    if case('pick'):
+      self.pick = tool
+      self.money -= cost
 player = Player()
 oak = Item(player, 'Oak', 5, 3, 10)
 stone = Item(player, 'Stone', 0, 2, 1)
@@ -73,17 +84,21 @@ oak.harvest()
 oak.sell('4')
 oak.sell('all')
 message = input()
-args = message.split()
-try:
-  if message.startswith('!'):
-    args[0] = args[0][1:]
-    switch(args[0])
-    if case('sell'):
-      switch(args[1].lower())
-      if case('oak'):
-        oak.sell(''.join(args[2:]))
-      elif case('stone'):
-        stone.sell(''.join(args[2:]))
-except IndexError:
-  error += '\nInput not long enough'
+while not message == 'stop':
+  args = message.split()
+  try:
+    if message.startswith('!'):
+      args[0] = args[0][1:]
+      switch(args[0])
+      if case('m'):
+        pass
+      elif case('sell'):
+        switch(args[1].lower())
+        if case('oak'):
+          oak.sell(''.join(args[2:]))
+        elif case('stone'):
+          stone.sell(''.join(args[2:]))
+  except IndexError:
+    error += '\nInput not long enough'
+  message = input()
 print('Errors:{}'.format(error))
