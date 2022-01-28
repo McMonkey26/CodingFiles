116ea1239ad586ab1802b023e353498ffb4cc41d
McMonkey26 <julian@pollackfamily.net>
2022-01-28 11:55:21 -0500

    changed nothing

--- PythonFiles/carGame/car.py
+++ PythonFiles/carGame/car.py
@@ -5,18 +5,28 @@ class Car(pygame.sprite.Sprite):
     self.image = pygame.Surface((100, 50))
     self.image = pygame.image.load('/Users/jpollack/Desktop/CodingFiles/repos/testFile/CodingFiles/PythonFiles/carGame/carImage.png').convert_alpha()
     self.image = pygame.transform.scale(self.image, (100, 50))
+    self.rotImage = self.image
     self.rect = self.image.get_rect()
     self.rect.topleft = (x, y)
-    self.velo = 10
+    self.pos = [x, y]
+    self.velo = 1
     self.angle = 0
   def turn(self, angle):
     self.angle += angle
-    self.rotImage = pygame.transform.rotate(self.image, self.angle)
+    center = rot_center(self.image, self.angle, self.rect.x, self.rect.y)
+    self.rotImage = center[0]
+    self.rotRect = center[1]
+    print(self.rect.center)
+    print(self.rotRect.center)
   def move(self):
-    self.rect.x += self.velo * math.cos(math.radians(self.angle))
-    self.rect.y -= self.velo * math.sin(math.radians(self.angle))
+    self.pos[0] += self.velo * math.cos(math.radians(self.angle))
+    self.pos[1] -= self.velo * math.sin(math.radians(self.angle))
+    self.rect.topleft = self.pos
 print(math.cos(math.radians(90)))
-
+def rot_center(image, angle, x, y):
+  rotated_image = pygame.transform.rotate(image, angle)
+  new_rect = rotated_image.get_rect(center = image.get_rect(center = (x, y)).center)
+  return rotated_image, new_rect
 pygame.init()
 screen = pygame.display.set_mode((600, 600))
 screen.fill((0, 0, 0))
