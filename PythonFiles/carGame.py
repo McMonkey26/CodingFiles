cad836a53e1d7d0c37e8c7746df36821fc2289a8
McMonkey26 <julian@pollackfamily.net>
2022-02-04 10:53:58 -0500

    added base collision, added coins, added death, added end screen

--- PythonFiles/carGame.py
+++ PythonFiles/carGame.py
@@ -8,7 +8,8 @@ pygame.init()
 screen = pygame.display.set_mode(((width:=600), (height:=750)))
 screen.fill([0, 0, 0])
 
-speed = 3
+speed = 10
+crashed = False
 
 class Road(pygame.sprite.Sprite):
   def __init__(self, yLevel):
@@ -27,14 +28,41 @@ background.add(Road(0))
 background.add(Road(600))
 background.add(Road(1200))
 background.add(Road(1800))
-class Zombie(pygame.sprite.Sprite):
+class Base(pygame.sprite.Sprite):
   def __init__(self, x, y):
     super().__init__()
-    self.image = pygame.Surface((50, 90))
-    self.image.fill((255, 0, 0))
+    self.image = pygame.Surface((80, 80))
+    self.image = pygame.image.load('/Users/jpollack/Desktop/CodingFiles/repos/testFile/CodingFiles/PythonFiles/base.png').convert_alpha()
+    self.image = pygame.transform.scale(self.image, (80,80))
     self.rect = self.image.get_rect()
     self.rect.center = (x, y)
-zombies = pygame.sprite.Group()
+  def update(self):
+    self.rect.y += speed
+    if self.rect.y >= height:
+      carSprite.points += 1
+      carSprite.coinText = pygame.font.SysFont('arial', 30).render('Coins: {}'.format(carSprite.coins), True, (100, 255, 100))
+      carSprite.coinRect = carSprite.coinText.get_rect(topright=(width, 0))
+      carSprite.pointText = pygame.font.SysFont('arial', 30).render('Points: {}'.format(carSprite.points), True, (100, 255, 100))
+      carSprite.pointRect = carSprite.pointText.get_rect(topleft=(0, 0))
+      try:
+        pygame.time.set_timer(pygame.USEREVENT, (2000//carSprite.points) if carSprite.points < 8 else 250)
+      except ZeroDivisionError:
+        pygame.time.set_timer(pygame.USEREVENT, 2000)
+      self.kill()
+bases = pygame.sprite.Group()
+class Coin(pygame.sprite.Sprite):
+  def __init__(self, x, y):
+    super().__init__()
+    self.image = pygame.Surface((40, 40))
+    self.image = pygame.image.load('/Users/jpollack/Desktop/CodingFiles/repos/testFile/CodingFiles/PythonFiles/rings.png').convert_alpha()
+    self.image = pygame.transform.scale(self.image, (40, 40))
+    self.rect = self.image.get_rect()
+    self.rect.center = (x, y)
+  def update(self):
+    self.rect.y += speed
+    if pygame.sprite.spritecollide(self, bases, False) or self.rect.y >= height:
+      self.kill()
+coins = pygame.sprite.Group()
 class Car(pygame.sprite.Sprite):
   def __init__(self, x, y):
     super().__init__()
@@ -44,6 +72,20 @@ class Car(pygame.sprite.Sprite):
     self.rect = self.image.get_rect()
     self.rect.center = (x, y)
     self.velo = [0, 0]
+    self.crashed = False
+    self.coins = 0
+    self.points = 0
+    self.coinText = pygame.font.SysFont('arial', 30).render('Coins: {}'.format(self.coins), True, (100, 255, 100))
+    self.coinRect = self.coinText.get_rect(topright=(width, 0))
+    self.pointText = pygame.font.SysFont('arial', 30).render('Points: {}'.format(self.points), True, (100, 255, 100))
+    self.pointRect = self.pointText.get_rect(topleft=(0, 0))
+  def collide(self, group):
+    return pygame.sprite.spritecollide(self, group, False)
+  def crash(self):
+    global crashed
+    self.image = pygame.image.load('/Users/jpollack/Desktop/CodingFiles/repos/testFile/CodingFiles/PythonFiles/deadGus.png').convert_alpha()
+    self.image = pygame.transform.scale(self.image, (100, 180))
+    crashed = True
   def move(self):
     key = pygame.key.get_pressed()
     if key[pygame.K_a]:
@@ -74,23 +116,79 @@ class Car(pygame.sprite.Sprite):
     if self.rect.bottom > height:
       self.rect.bottom = height
       self.velo[1] = 0
+    if self.collide(bases):
+      self.crash()
+    for hit in self.collide(coins):
+      hit.kill()
+      self.coins += 1
+    self.coinText = pygame.font.SysFont('arial', 30).render('Coins: {}'.format(self.coins), True, (100, 255, 100))
+    self.coinRect = self.coinText.get_rect(topright=(width, 0))
+    self.pointText = pygame.font.SysFont('arial', 30).render('Points: {}'.format(self.points), True, (100, 255, 100))
+    self.pointRect = self.pointText.get_rect(topleft=(0, 0))
 carSprite = Car(300, 600)
 pygame.time.set_timer(pygame.USEREVENT, 2000)
+pygame.time.set_timer(pygame.USEREVENT+1, 500)
 
-run = True
-#main loop
-while run:
-  #allows you to press the x in the top corner of pygame window and close it
+def gameLoop():
+  global run, frame, clock
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       run = False
     if event.type == pygame.USEREVENT:
-      zombies.add(Zombie(random.randint(0, width), random.randint(0, height)))
+      bases.add(Base(random.randint(0, width), -60))
+    if event.type == pygame.USEREVENT+1:
+      coins.add(Coin(random.randint(0, width), -30))
   #updates the screen for the user with anything that happened in the evnets
   screen.fill((0, 0, 0))
   carSprite.update()
   background.update()
+  bases.update()
+  coins.update()
   background.draw(screen)
-  zombies.draw(screen)
+  bases.draw(screen)
+  coins.draw(screen)
   screen.blit(carSprite.image, carSprite.rect.topleft)
+  screen.blit(carSprite.pointText, carSprite.pointRect.topleft)
+  screen.blit(carSprite.coinText, carSprite.coinRect.topleft)
+  # screen.blit(pygame.font.SysFont('arial', 30).render(str(int(clock.get_fps())), True, (100, 255, 100)), pygame.font.SysFont('arial', 30).render(str(int(clock.get_fps())), True, (100, 255, 100)).get_rect(topleft=(0, 0)).topleft)
+def winScreen():
+  global run, crashed
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_SPACE:
+        crashed = False
+        carSprite.points = 0
+        carSprite.coins = 0
+        for sprite in bases:
+          sprite.kill()
+        for sprite in coins:
+          sprite.kill()
+        carSprite.rect.center = (width/2, height/2)
+        carSprite.image = pygame.image.load('/Users/jpollack/Desktop/CodingFiles/repos/testFile/CodingFiles/PythonFiles/guschillin.png').convert_alpha()
+        carSprite.image = pygame.transform.scale(carSprite.image, (100, 180))
+        carSprite.coinText = pygame.font.SysFont('arial', 30).render('Coins: {}'.format(carSprite.coins), True, (100, 255, 100))
+        carSprite.coinRect = carSprite.coinText.get_rect(topright=(width, 0))
+        carSprite.pointText = pygame.font.SysFont('arial', 30).render('Points: {}'.format(carSprite.points), True, (100, 255, 100))
+        carSprite.pointRect = carSprite.pointText.get_rect(topleft=(0, 0))
+  screen.fill((0, 0, 0))
+  background.draw(screen)
+  bases.draw(screen)
+  screen.blit(carSprite.image, carSprite.rect.topleft)
+  screen.blit(pygame.font.SysFont('arial', 40).render('You evaded {} base{}.'.format(carSprite.points, '' if carSprite.points == 1 else 's'), True, (255, 255, 255)), pygame.font.SysFont('arial', 40).render('You evaded {} base{}.'.format(carSprite.points, '' if carSprite.points == 1 else 's'), True, (255, 255, 255)).get_rect(center=(width/2, height/2)).topleft)
+  screen.blit(pygame.font.SysFont('arial', 40).render('You got {} coin{}.'.format(carSprite.coins, '' if carSprite.coins == 1 else 's'), True, (255, 255, 255)), pygame.font.SysFont('arial', 40).render('You got {} coin{}'.format(carSprite.coins, '' if carSprite.coins == 1 else 's'), True, (255, 255, 255)).get_rect(center=(width/2, height/2+40)).topleft)
+  screen.blit(pygame.font.SysFont('arial', 40).render('Try getting good.', True, (255, 255, 255)), pygame.font.SysFont('arial', 40).render('Try getting good.', True, (255, 255, 255)).get_rect(center=(width/2, height/2+80)).topleft)
+  screen.blit(pygame.font.SysFont('arial', 40).render('Press SPACE to restart', True, (255, 255, 255)), pygame.font.SysFont('arial', 40).render('Press SPACE to restart', True, (255, 255, 255)).get_rect(center=(width/2, height/2+120)).topleft)
+run = True
+clock = pygame.time.Clock()
+frame = 0
+#main loop
+while run:
+  clock.tick(60)
+  frame += 1
+  if not crashed:
+    gameLoop()
+  else:
+    winScreen()
   pygame.display.flip()
