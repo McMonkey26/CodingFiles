e0bcb1270c41f601ca3a6c3a2a05b0c96961e42d
McMonkey26 <julian@pollackfamily.net>
2022-02-06 15:55:17 -0500

    i just made wordle. for fun. you know

---
+++ PythonFiles/wordle.py
@@ -0,0 +1,62 @@
+import random, os, urllib.request, ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+os.chdir('/Users/jpollack/Desktop/CodingFiles/repos/testFile/CodingFiles/PythonFiles')
+def getDictFromURL():
+  url = 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt'
+  file = urllib.request.urlopen(url)
+  wordList = []
+  for line in file:
+    decoded_line = line.decode("utf-8").rstrip('\n')
+    if len(decoded_line) == 5 and all(list(map(lambda x: x in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', decoded_line))):
+      wordList.append(decoded_line.upper())
+  return wordList
+def getDictFromFile():
+  with open('5LetterDict.txt', 'r') as file:
+    wordList = list(map(lambda x:x.rstrip('\n'), file))
+  return wordList
+def checkWord(goal, userInput):
+  goal = list(goal)
+  returnWord = ''
+  for letter in range(len(userInput)):
+    if userInput[letter] in goal:
+      if userInput[letter] == goal[letter]:
+        returnWord += userInput[letter]
+      else:
+        returnWord += userInput[letter].lower()
+      goal[goal.index(userInput[letter])] = '_'
+    else:
+      returnWord += '_'
+  return returnWord
+def getPos(word, dictionary):
+  dictList = [x for x in dictionary]
+  for entry in dictionary:
+    for letter in range(5):
+      if word[letter].islower():
+        if not word[letter].upper() in entry:
+          dictList.remove(entry)
+          break
+      else:
+        if not(word[letter] == entry[letter] or word[letter] == '_'):
+          dictList.remove(entry)
+          break
+  return dictList
+def getInput(dictionary):
+  userInput = input()
+  while not userInput.upper() in dictionary:
+    if userInput.startswith('checkword'):
+      print(getPos(userInput.split(' ')[1], dictionary))
+      userInput = input()
+    else:
+      print('That is not a valid word.')
+      userInput = input('Please input a 5 letter word.\n')
+  return userInput.upper()
+wordList = getDictFromFile()
+tries = 1
+dailyWord = random.choice(wordList).upper()
+inputWord = getInput(wordList)
+print(checkWord(dailyWord, inputWord))
+while not inputWord == dailyWord:
+  tries += 1
+  inputWord = getInput(wordList)
+  print(checkWord(dailyWord, inputWord))
+print('It took you {} tries. Casual'.format(tries))
