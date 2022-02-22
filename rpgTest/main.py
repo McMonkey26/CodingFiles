5845b7e2e77ac1b429e1196dac9fdc22d980cc6d
McMonkey26 <julian@pollackfamily.net>
2022-02-22 08:45:29 -0500

    created dummy enemy and implemented multiple words

--- rpgTest/main.py
+++ rpgTest/main.py
@@ -1,9 +1,8 @@
 import players, pygame
-
-tileSize = 16
+from constants import *
 pygame.init()
 
-screen = pygame.display.set_mode((tileSize*15, tileSize*7))
+screen = pygame.display.set_mode((tileSize*15, tileSize*10))
 screen.fill((255, 255, 255))
 
 #| # -> wall
@@ -94,28 +93,6 @@ enemyMaps = [
   [pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()],
   [pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()]
 ]
-for x in range(len(worldMaps)):
-  for y in range(len(worldMaps[x])):
-    for line in range(len(worldMaps[x][y])):
-      for tile in range(len(worldMaps[x][y][line])):
-        if worldMaps[x][y][line][tile] == '#':
-          tempVariable = Wall(tile, line)
-          wallMaps[x][y].add(tempVariable)
-        elif worldMaps[x][y][line][tile] == 'C':
-          tempVariable = CoinChest(tile, line)
-          chestMaps[x][y].add(tempVariable)
-        elif worldMaps[x][y][line][tile] == 'c':
-          tempVariable = KeyChest(tile, line, 'testingtesting123')
-          chestMaps[x][y].add(tempVariable)
-        elif worldMaps[x][y][line][tile] == 'S':
-          tempVariable = Slime(tile, line, 2)
-          enemyMaps[x][y].add(tempVariable)
-        elif worldMaps[x][y][line][tile] == 's':
-          tempVariable = Slime(tile, line, 1)
-          enemyMaps[x][y].add(tempVariable)
-        else:
-          continue
-        spriteMaps[x][y].add(tempVariable)
 class Wall(pygame.sprite.Sprite):
   def __init__(self, x, y):
     super().__init__()
@@ -156,15 +133,59 @@ class Slime(pygame.sprite.Sprite):
     self.rect = self.image.get_rect()
     self.rect.topleft = (x*tileSize, y*tileSize)
     self.type = 'Enemy'
-    self.damage = 4
-    self.health = 100
+    self.damage = 4*size
+    self.health = 50*size
+    self.size = size
     self.alive = True
-  def interact(self):
+  def interact(self, player):
     if self.health <= 0:
-      self.die()
-  def die(self):
+      self.die(player)
+  def die(self, player):
     self.image.fill((66, 42, 5))
     self.alive = False
+    player.xp += 10*self.size
+class Dummy(pygame.sprite.Sprite):
+  def __init__(self, x, y):
+    super().__init__()
+    self.image = pygame.Surface((tileSize*3, tileSize*3))
+    self.image.fill((180, 180, 180))
+    self.rect = self.image.get_rect()
+    self.rect.topleft = (x*tileSize, y*tileSize)
+    self.health = 100
+    self.text = pygame.font.SysFont('arial', int(1.25*tileSize)).render(str(self.health), True, (255, 255, 255))
+    self.textRect = self.text.get_rect(center=(1.5*tileSize, 1.5*tileSize))
+    self.image.blit(self.text, self.textRect)
+    self.type = 'NPC'
+    self.damage = 0
+    self.alive = True
+  def interact(self, player):
+    if self.health <= 0: self.health = 100
+    self.image.fill((180, 180, 180))
+    self.text = pygame.font.SysFont('arial', int(1.25*tileSize)).render(str(self.health), True, (255, 255, 255))
+    self.textRect = self.text.get_rect(center=(1.5*tileSize, 1.5*tileSize))
+    self.image.blit(self.text, self.textRect)
+for x in range(len(worldMaps)):
+  for y in range(len(worldMaps[x])):
+    for line in range(len(worldMaps[x][y])):
+      for tile in range(len(worldMaps[x][y][line])):
+        if worldMaps[x][y][line][tile] == '#':
+          tempVariable = Wall(tile, line)
+          wallMaps[x][y].add(tempVariable)
+        elif worldMaps[x][y][line][tile] == 'C':
+          tempVariable = CoinChest(tile, line)
+          chestMaps[x][y].add(tempVariable)
+        elif worldMaps[x][y][line][tile] == 'c':
+          tempVariable = KeyChest(tile, line, 'testingtesting123')
+          chestMaps[x][y].add(tempVariable)
+        elif worldMaps[x][y][line][tile] == 'S':
+          tempVariable = Slime(tile, line, 2)
+          enemyMaps[x][y].add(tempVariable)
+        elif worldMaps[x][y][line][tile] == 's':
+          tempVariable = Slime(tile, line, 1)
+          enemyMaps[x][y].add(tempVariable)
+        else:
+          continue
+        spriteMaps[x][y].add(tempVariable)
 all_sprites = pygame.sprite.Group()
 walls = pygame.sprite.Group()
 chests = pygame.sprite.Group()
@@ -189,18 +210,25 @@ for line in range(len(world)):
     else:
       continue
     all_sprites.add(tempVariable)
+tempVariable = Dummy(0, 7)
+all_sprites.add(tempVariable)
+enemies.add(tempVariable)
+julian = players.adventurer('Hawk Feather', 'Tabaxi', 81, [players.fight, players.elvenBow, players.flight, players.bomb], 'Hawk')
 def gameLoop():
-  global tempStm
-  # screen.fill((255, 255, 255))
+  # screen.fill((128, 128, 128))
   all_sprites.draw(screen)
-  screen.blit(players.julian.image, players.julian.rect.topleft)
-  players.julian.stm += players.julian.maxStm/2000
-  if players.julian.stm >= players.julian.maxStm: players.julian.stm = players.julian.maxStm
-  if not int(players.julian.stm) == tempStm:
-    print(int(players.julian.stm))
-    tempStm = int(players.julian.stm)
+  screen.blit(julian.image, julian.rect.topleft)
+  screen.blit(julian.inv.image, (0, tileSize*7))
+  players.testing.update(julian)
+  screen.blit(players.testing.image, (tileSize*12, tileSize*7))
   pygame.display.flip()
-tempStm = 100
+def worldLoop(world):
+  spriteMaps[world[1]][world[0]].draw(screen)
+  screen.blit(julian.image, julian.rect.topleft)
+  screen.blit(julian.inv.image, (0, tileSize*7))
+  players.testing.update(julian)
+  screen.blit(players.testing.image, (tileSize*12, tileSize*7))
+  pygame.display.flip()
 running = True
 while running:
   for event in pygame.event.get():
@@ -209,8 +237,9 @@ while running:
       break
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_SPACE:
-        players.julian.show()
-    screen.fill((255, 255, 255))
-    players.julian.move(event, all_sprites)
-    players.julian.ability(event, screen, enemies)
-  gameLoop()
+        julian.show()
+    screen.fill((128, 128, 128))
+    julian.move(event, spriteMaps[julian.world[1]][julian.world[0]])
+    julian.ability(event, screen, enemyMaps[julian.world[1]][julian.world[0]])
+  julian.update(screen, enemyMaps[julian.world[1]][julian.world[0]])
+  worldLoop(julian.world)
