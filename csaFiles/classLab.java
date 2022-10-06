a479d9131aabc8424e3c2f3b9a63a45cbcd57d96
McMonkey26 <julian@pollackfamily.net>
2022-10-06 14:05:57 -0400

    pearl is fucking weird

---
+++ csaFiles/classLab.java
@@ -0,0 +1,47 @@
+public class classLab {
+  public static void main(String[] args) {
+    class Cat {
+      private int age;
+      private String name;
+      private boolean longHair;
+      private String color;
+      private double weight;
+    
+      public Cat() {
+        age = 5;
+        name = "Fido";
+        longHair = false;
+        color = "Orange";
+        weight = 12.75;
+      }
+      public Cat(int age, String name, boolean longHair, String color, double weight) {
+        this.age = age;
+        this.name = name;
+        this.longHair = longHair;
+        this.color = color;
+        this.weight = weight;
+      }
+    
+      public int getAge() {return age;}
+      public String getName() {return name;}
+      public boolean getLongHair() {return longHair;}
+      public String getColor() {return color;}
+      public double getWeight() {return weight;}
+    
+      public void setAge(int age) {this.age = age;}
+      public void setName(String name) {this.name = name;}
+      public void setLongHair(boolean longHair) {this.longHair = longHair;}
+      public void setColor(String color) {this.color = color;}
+      public void setWeight(double weight) {this.weight = weight;}
+    
+      public void getDirty() {this.color = "Brown";}
+      public void meow() {System.out.println("Meow! " + this.name);}
+      public boolean isHealthy() {return (this.age < 4 && this.weight < 15.0);}
+      public String toString() {return String.format("age:%d, name:%s, longHair:%b, color:%s, weight:%f", age, name, longHair, color, weight);}
+    }
+    Cat kitty = new Cat();
+    kitty.meow();
+    System.out.println(kitty);
+  
+  }
+}
--- csaFiles/timesTableLab.java
+++ csaFiles/timesTableLab.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class timesTableLab {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int num = sc.nextInt();
-    for (int i = 1; i <= num; i++) {
-      for (int j = 1; j <= 10; j++) {
-        System.out.print("" + j * i + "\t");
+    Scanner sc = new Scanner(System.in); //creates a new scanner object to take input
+    int num = sc.nextInt(); //gets a number from the user
+    for (int i = 1; i <= num; i++) { //iterates up to the number
+      for (int j = 1; j <= 10; j++) { //iterates up to ten
+        System.out.print("" + j * i + "\t"); //prints out the current number multiplied by the value to ten, separated by tabs
       }
-      System.out.println();
+      System.out.println(); //goes to a new line
     }
-    sc.close();
+    sc.close(); //closes the scanner object for memory purposes
   }
 }
